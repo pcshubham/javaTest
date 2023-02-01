4622dd978f29d6f311a9aaea1c6799a026517e1b
pcshubham41 <pcshubham41@gmail.com>
2023-02-02 00:55:01 +0530

    first commit

---
+++ src/test/java/oddEvenTestProject/TestEvenNumber.java
@@ -0,0 +1,12 @@
+package oddEvenTestProject;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestEvenNumber {
+	@Test
+	public void testEvenNumber() {
+		assertTrue(OddEvenNumber.checkTheNumberIsEven(36));
+	}
+}
